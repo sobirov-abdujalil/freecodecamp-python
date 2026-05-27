9f1028d40e47f3604b7c0a2bfa750088a3f8d903
sobirov-abdujalil <abdujalilsobirovfronted@gmail.com>
2026-05-27 18:42:41 +0500

    update

--- Python Basics/Build a Bill Splitter/main.py
+++ Python Basics/Build a Bill Splitter/main.py
@@ -1,1 +1,15 @@
 print("Hello! Welcome to the tip calculator.")
+
+total = float(input("What is the total bill? $"))
+
+people = int(input("How many people are splitting the bill? "))
+
+tip_percent = float(input("What percentage tip would you like to leave? "))
+
+tip = total * (tip_percent / 100)
+
+grand_total = total + tip
+
+per_person = grand_total / people
+
+print(f"Each person pays: ${per_person:.2f}")
